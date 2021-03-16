55b05a52d4d64c69471879d0ace8fbb1ea28558d
ramk.balu <ramk.balu@gmail.com>
2021-03-16 22:26:45 +0000

    folder structure updated and solution name corrected

--- src/sample/SimulateQueue.cs
+++ src/sample/SimulateQueue.cs
@@ -2,29 +2,30 @@ using System;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Queue;
 
-namespace Simulate_queue
+namespace QueueHelper
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            string storageAccountConnectionString = "<<Enter your connectionstring>>";
+            // string storageAccountConnectionString = "<<Enter your connectionstring>>";
 
-            string queueName = "<<Entry your queue name here>>"; 
+            // string queueName = "<<Entry your queue name here>>"; 
 
             int messageCount = 100;
 
-            var storageAccount = CloudStorageAccount.Parse(storageAccountConnectionString);
+            // var storageAccount = CloudStorageAccount.Parse(storageAccountConnectionString);
 
-            var queueClient = storageAccount.CreateCloudQueueClient();
+            // var queueClient = storageAccount.CreateCloudQueueClient();
 
-            var queueReference = queueClient.GetQueueReference(queueName);
+            // var queueReference = queueClient.GetQueueReference(queueName);
 
-            queueReference.CreateIfNotExistsAsync();
+            // queueReference.CreateIfNotExistsAsync();
 
             for(int i=0; i < messageCount ;i++)
             {
-                queueReference.AddMessageAsync(new CloudQueueMessage($"my test message {i}"));
+                // queueReference.AddMessageAsync(new CloudQueueMessage($"my test message {i}"));
+                System.Console.WriteLine("test");
             }
 
             System.Console.WriteLine("Message sent sucessfully");
