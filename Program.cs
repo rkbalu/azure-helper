1710925b7504adb70ee42ab5d20bf9fe831c708a
ramk.balu <ramk.balu@gmail.com>
2021-03-16 21:33:20 +0000

    initial version

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
+
+namespace Simulate_queue
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string storageAccountConnectionString = "Enter your connectionstring";
+
+            string queueName = "Entry your queue name here"; 
+
+            int messageCount = 100;
+
+            System.Console.WriteLine("Enter your Queue name?");
+
+            var storageAccount = CloudStorageAccount.Parse(storageAccountConnectionString);
+
+            var queueClient = storageAccount.CreateCloudQueueClient();
+
+            var queueReference = queueClient.GetQueueReference(queueName);
+
+            queueReference.CreateIfNotExistsAsync();
+
+            for(int i=0; i < messageCount ;i++)
+            {
+                queueReference.AddMessageAsync(new CloudQueueMessage($"my test message {i}"));
+            }
+
+            System.Console.WriteLine("Message sent sucessfully");
+            
+        }
+    }
+}
